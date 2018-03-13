809cd8a7d73140c67cbd98128cacd485ac11ec71
Peter Norman <october8th@hotmail.com>
2018-03-13 10:12:46 -0400

    notes

--- public/js/app.js
+++ public/js/app.js
@@ -99,7 +99,7 @@ function showSaved(data)
     myHTML += "<img style='border:1px solid gray;width:150px;height:100px; float:right' src=" + data[i].image +">";
     myHTML += "<p class='card-text'>" + data[i].story+ "</p>";
     myHTML += "<a href='" + data[i].link + "'" +"class='btn btn-primary' target='_blank'>View Article</a>";
-    myHTML += "<a href='#' data-id='" + data[i]._id + "' class='btn btn-default'>Comment</a></div></div>";
+    myHTML += "<a href='#' data-id='" + data[i]._id + "' class='btn btn-default'>Comment</a>";
     myHTML += "<a href='#' data-id='" + data[i]._id + "' class='btn btn-danger'>Delete Article</a></div></div>";
     $("#main").append(myHTML);
     //.append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].image +data[i].link + data[i].story + "<br />" + "</p>");
@@ -163,7 +163,7 @@ $(document).on("click", ".btn-success", function()
     });
 });
 
-$(document).on('click', '#btn-default', (function() 
+$(document).on('click', '.btn-default', (function() 
 {
   // Empty the notes from the note section
   $("#note-section").empty();
